9a581914456db911e46de7f0072bb6b7aac50d45
Eric Feng <erichfeng@gmail.com>
2017-06-09 23:33:02 -0700

    update requirements

--- README.md
+++ README.md
@@ -6,10 +6,4 @@ For piping little data into Postgres.
 
 Requires environment variable:
 
-- `ZENDESK_EMAIL`
-- `ZENDESK_API_KEY`
-- `ZENDESK_SUBDOMAIN`
-
-In development, pass `DEVELOPMENT_DOMAIN` to use ngrok. Zendesk doesn't accept localhost target urls.
-
-`export FLASK_APP=app.py flask run`
+- `PIPET_DOMAIN=ehfeng.ngrok.io python run.py`
--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask
 from flask_login import LoginManager, current_user
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form, fields, validators, Required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://eric:@localhost/pipet'
@@ -21,10 +22,22 @@ app.register_blueprint(admin)
 from app.zendesk import zendesk
 app.register_blueprint(zendesk, url_prefix='/zendesk')
 
+
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+
+
+class LoginForm(Form):
+	name = fields.TextField(validators=[Required()])
+
 @app.route('/')
 def index():
     return "Welcome to Pipet"
 
-@app.route('/login')
+
+@app.route('/login', methods=('GET', 'POST'))
 def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		return redirect(request.args.get("next") or url_for("index"))
 	return "login page"
