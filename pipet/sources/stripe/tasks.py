73eb0f8c3d60015af97f84db609b8a921485e2eb
Eric Feng <erichfeng@gmail.com>
2018-05-20 23:41:58 -0700

    backfill and updates must be different for Stripe

--- pipet/sources/stripe/tasks.py
+++ pipet/sources/stripe/tasks.py
@@ -1,31 +1,57 @@
-from celery_once import QueueOnce
 from inspect import isclass
-from sqlalchemy.orm.attributes import flag_modified
-import stripe
+
+from celery_once import QueueOnce
 import requests
+import stripe
 
 from pipet import celery, db
 from pipet.sources.stripe import StripeAccount
 from pipet.sources.stripe.models import (
     Base,
     CLASS_REGISTRY,
+    EmptyResponse,
 )
 
 
 @celery.task(base=QueueOnce)
 def sync(account_id):
+    # backfill or update
+    pass
+
+
+def update(account, event_id=None):
+    if not event_id:
+        resp = account.get('/v1/events',
+                           params={'starting_after': account.get_cursor('events')})
+        resp.raise_for_status()
+        data = resp.json()['data']
+        if not data:
+            # No additional events
+            return
+        else:
+            event_id = data['id']
+    # TODO do sync work
+
+
+def backfill(account):
+    """
+    TODO https://www.ehfeng.com/mirroring-stripe/
+    """
+    resp = account.get('/v1/events', params={'limit': 1})
+    resp.raise_for_status()
+    event_id = resp.json()['data'][0]['id']
+
     account = StripeAccount.query.get(account_id)
     session = account.organization.create_session()
 
-    for cls in [m for n, m in CLASS_REGISTRY.items() if isclass(m) and issubclass(m, Base)]:
-        # Make these parallel to speed up execution
+    for cls in [m for n, m in CLASS_REGISTRY.items() if isclass(m) and issubclass(m, Base) and m.endpoint]:
+        # TODO Make these parallel to speed up execution
         while True:
             conn = session.connection()
-            statements, cursor, has_more = cls.sync(account)
-            print(cls)
-            print(statements)
-            account.cursors[cls.__tablename__] = cursor
-            flag_modified(account, 'cursors')
+            try:
+                statements, cursor, has_more = cls.sync(account)
+            except EmptyResponse:
+                break
 
             for statement in statements:
                 conn.execute(statement)
